254182fff71314aa89f41fa65276523f399b8a35
Kaleemullah-Masjid <140999688+Kaleemullah-Masjid@users.noreply.github.com>
2023-08-03 22:00:05 -0500

    Update Times.js

--- Times.js
+++ Times.js
@@ -18,12 +18,22 @@ async function getSunriseSunsetTime(latitude, longitude) {
 }
 // Function to display the sunrise and sunset times on the HTML page
 function displaySunriseSunsetTimes(sunriseTime, sunsetTime) {
-    const sunriseSunsetInfoDiv = document.getElementById('Prayer_Times');
-    sunriseSunsetInfoDiv.innerHTML = `
-        <h1>Sunrise and Sunset Times</h1>
-        <p>Sunrise: ${sunriseTime}</p>
-        <p>Sunset: ${sunsetTime}</p>
-    `;
+    const table = document.querySelector('#sunrise-sunset-table');
+    const headerRow = table.querySelector('thead tr');
+    const dataRows = table.querySelectorAll('tbody tr');
+
+    const newHeaderCell = document.createElement('th');
+    newHeaderCell.textContent = 'Sunrise';
+    headerRow.insertBefore(newHeaderCell, headerRow.children[0]);
+
+    const newDataCell = document.createElement('td');
+    newDataCell.textContent = '${sunriseTime}'
+    dataRows.insertBefore(newDataCell, dataRows.children[10]);
+    //sunriseSunsetInfoDiv.innerHTML = '${sunriseTime}'
+    //<h1>Sunrise and Sunset Times</h1>
+    //<p>Sunrise:}</p>
+    //<p>Sunset: ${sunsetTime}</p>
+    
 }
 // Function to display the sunrise and sunset times
 function displaySunriseSunsetTime(data) {
