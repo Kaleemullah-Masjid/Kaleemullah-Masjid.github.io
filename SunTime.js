fb9e35e874dab608c7173f5b95c7c186783c85ca
Kaleemullah-Masjid <140999688+Kaleemullah-Masjid@users.noreply.github.com>
2023-08-05 23:21:14 -0500

    Update SunTime.js

--- SunTime.js
+++ SunTime.js
@@ -14,6 +14,70 @@ async function getSunriseSunsetTime() {
         return null;
     }
 }
+function getCurrentFormattedDate() {
+    const months = [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = months[today.getMonth()];
+    const year = today.getFullYear();
+
+    return `${day}-${month}-${year}`;
+}
+
+function getFormattedDate() {
+    const today = new Date();
+
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1; // Months are zero-based, so add 1
+    const day = today.getDate();
+    //Output: "2023-08-03" (current date will vary)
+    //const formattedDate = `${year}-${month < 10 ? '0' : ''}${month}-${day < 10 ? '0' : ''}${day}`;
+    //05-08-2023 DD-MM-YYYY
+    const formattedDate = `${day < 10 ? '0' : ''}${day}-${month < 10 ? '0' : ''}${month}-${year}`;
+
+    return formattedDate;
+}
+async function getIslamicDate() {
+    const date = getFormattedDate(); // Replace with the desired Gregorian date
+
+    //Example Request: http://api.aladhan.com/v1/gToH/07-12-2014
+    const apiUrl = `https://api.aladhan.com/v1/gToH?date=${date}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (response.ok && data.code === 200) {
+            const hijri_year = data.data.hijri.year;
+            const hijri_day = data.data.hijri.day;
+            const hijri_month = data.data.hijri.month.number;
+            const hijri_month_str = data.data.hijri.month.en;
+            const islamicDate_1 = data.data.hijri.date
+            const islamicDate_2 = `${hijri_day}-${hijri_month_str}-${hijri_year}`;
+            const islamicDate = {islamicDate_1,islamicDate_2}
+            return islamicDate;
+        } else {
+            console.error('Failed to fetch Islamic date.');
+            return null;
+        }
+    } catch (error) {
+        console.error('Error fetching Islamic date:', error);
+        return null;
+    }
+}
+// Function to display the sunrise and sunset times on the HTML page
+function displayHijriDate(Hijri_date_t) {
+    const Hijri_date_Cell = document.getElementById('Hijri');
+    const formattedDate = getCurrentFormattedDate();
+    Hijri_date_Cell.innerHTML = `<b>Today is: </b> ${formattedDate} || ${Hijri_date_t}`;
+    
+}
+
+
+
 // Function to display the sunrise and sunset times on the HTML page
 function displaySunriseSunsetTimes(sunriseTime, sunsetTime) {
     const sunsetTimeCell = document.getElementById('sunsetTime');
@@ -25,16 +89,24 @@ function displaySunriseSunsetTimes(sunriseTime, sunsetTime) {
 // Function to display the sunrise and sunset times
 async function loadSunriseSunsetData(data) {
     const sunriseSunsetData = await getSunriseSunsetTime();
+    const islamic_dates = await getIslamicDate();
     if (sunriseSunsetData) {
-        const sunriseTime = new Date(sunriseSunsetData.sunrise).toLocaleTimeString();
-        const sunsetTime = new Date(sunriseSunsetData.sunset).toLocaleTimeString();
-        displaySunriseSunsetTimes(sunriseTime, sunsetTime);
-        console.log('Sunrise Time:', sunriseTime);
-        console.log('Sunset Time:', sunsetTime);
+        if (islamic_dates) {
+            const islamic_date = islamic_dates.islamicDate_2
+            const sunriseTime = new Date(sunriseSunsetData.sunrise).toLocaleTimeString();
+            const sunsetTime = new Date(sunriseSunsetData.sunset).toLocaleTimeString();
+            
+            displayHijriDate(islamic_date);
+            displaySunriseSunsetTimes(sunriseTime, sunsetTime);
+        } else {z
+            console.log('Unable to fetch Islamic Date data.');
+        }
         
     } else {
-        console.log('Unable to fetch sunrise-sunset data.');
+            console.log('Unable to fetch sunrise-sunset data.');
     }
 }
+
+
 // Call the main function when the page finishes loading
 window.addEventListener('load', loadSunriseSunsetData);
